942099d0c41a6bfd0bb2ff5bdf200fb006e8cd87
MiifPadilha <mii.f.padilha@gmail.com>
2023-08-21 21:40:30 -0300

    crud finalizado

--- classes/Crud.php
+++ classes/Crud.php
@@ -1,82 +1,98 @@
 <?php
 include_once('conexao/conexao.php');
 
-$db = new database();
+$db = new Database();
 
-class crud{
+class Crud{
     private $conn;
     private $table_name = "carros";
 
     public function __construct($db){
         $this->conn = $db;
     }
-//funcao paraCriar seu registro
-public function create($postValues){
-    $modelo = $postValues['modelo'];
-    $marca = $postValues['marca'];
-    $placa = $postValues['placa'];
-    $cor = $postValues['cor'];
-    $ano = $postValues['ano'];
 
-    $query = "INSERT ". $this->table_name . " (modelo,marca,placa,cor,ano)  values (?,?,?,?,?)";
-    $stmt = $this->conn->prepare($query);
-    $stmt->bindParam(1,$marca);
-    $stmt->bindParam(2,$modelo);
-    $stmt->bindParam(3,$placa);
-    $stmt->bindParam(4,$cor);
-    $stmt->bindParam(5,$ano);
+    public function create($postValues){
+        $modelo = $postValues['modelo'];
+        $marca = $postValues['marca'];
+        $placa = $postValues['placa'];
+        $cor = $postValues['cor'];
+        $ano = $postValues['ano'];
 
-    $rows = $this->read();
+        $query = "INSERT INTO ". $this->table_name . " (modelo, marca, placa, cor, ano) VALUES (?,?,?,?,?)";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(1,$modelo);
+        $stmt->bindParam(2,$marca);
+        $stmt->bindParam(3,$placa);
+        $stmt->bindParam(4,$cor);
+        $stmt->bindParam(5,$ano);
+
+        $rows = $this->read();
+        if($stmt->execute()){
+            print "<script>alert('Cadastro Ok!')</script>";
+            print "<script> location.href='?action=read'; </script>";
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public function read(){
+        $query = "SELECT * FROM ". $this->table_name;
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        return $stmt;
+    }
+
+    public function update($postValues){
+        
+        $id = $postValues['id'];
+        $modelo = $postValues['modelo'];
+        $marca = $postValues['marca'];
+        $placa = $postValues['placa'];
+        $cor = $postValues['cor'];
+        $ano = $postValues['ano'];
+
+        if(empty($id) || empty($modelo) || empty($marca) || empty($placa) || empty($cor) || empty($ano)){
+            return false;
+        }
+        
+        $query = "UPDATE ". $this->table_name . " SET modelo = ?, marca = ?, placa = ?, cor = ?, ano = ? WHERE id = ?";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(1,$modelo);
+        $stmt->bindParam(2,$marca);
+        $stmt->bindParam(3,$placa);
+        $stmt->bindParam(4,$cor);
+        $stmt->bindParam(5,$ano);
+        $stmt->bindParam(6,$id);
+        if($stmt->execute()){
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+        public function readOne($id){
+            $query = "SELECT * FROM ". $this->table_name . " WHERE id = ?";
+            $stmt = $this->conn->prepare($query);
+            $stmt->bindParam(1, $id);
+            $stmt->execute();
+            return $stmt->fetch(PDO::FETCH_ASSOC);
+
+        }
+
+        //função para apagar os registros
+public function delete ($id){
+    $query = "DELETE FROM ". $this->table_name . " WHERE id = ?";
+    $stmt= $this->conn->prepare($query);
+    $stmt->bindParam(1,$id);
     if($stmt->execute()){
-        print "<script>alert('cadastro ok!')</script>";
-        print "<script> location.href='?action=red';</script>";
         return true;
     }else{
         return false;
-
     }
 
-
 }
 
-//funcao ler o registro
-public function read(){
-    $query = "SELECT * FROM " . $this->table_name;
-    $stmt = $this->conn->prepare($query);
-    $stmt->execute();
-    return $stmt;
-}
 
- //funcao atualizar registros
- public function  update($postValues){
-    $modelo = $postValues['modelo'];
-    $marca = $postValues['marca'];
-    $placa = $postValues['placa'];
-    $cor = $postValues['cor'];
-    $ano = $postValues['ano'];
-
-    if(empty($id) || empty($marca) || empty($modelo) || empty($placa) || empty($cor) || empty($ano)){
-        return false;
     }
-    $query = "UPDATE ".$this->table_name . " SET marca = ?, modelo = ?, placa = ?, cor = ?, ano = ? WHERE id = ?";
-    $stmt =  $this-> conn->pepare($query);
-    $stmt->bindParam(1,$marca);
-    $stmt->bindParam(2,$modelo);
-    $stmt->bindParam(3,$placa);
-    $stmt->bindParam(4,$cor);
-    $stmt->bindParam(5,$ano);
-    if($stmt->execute()){
-        return true;
-    }else{
-        return false;
-    }
- }
-  // função para registrar o banco e inderir o formulário
-  public function readOne($id) {
-    $query = "SELECT * FROM ". $this-> table_name . "WHERE id = ?";
-    $stmt = $this ->conn->prepare($query);
-    $stmt
-}
-
-}
 ?>
--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php
+
 require_once('classes/Crud.php');
 require_once('conexao/conexao.php');
 
-$database = new Database();
+$database = new Database ();
 $db = $database->getConnection();
-$crud = new Crud($db);
+$crud =new Crud ($db);
+
 
 if(isset($_GET['action'])){
     switch($_GET['action']){
@@ -12,24 +14,35 @@ if(isset($_GET['action'])){
             $crud->create($_POST);
             $rows = $crud->read();
             break;
+        case 'read';
+            $rows = $crud->read();
+            break;
+        case 'update':
+                if(isset($_POST['id'])){
+                    $crud->update($_POST);
 
-            case'read':
+                }
                 $rows = $crud->read();
-                break;
+                break;  
+        //case delete
+        case 'delete';
+        $crud->delete($_GET['id']);
+        $rows = $crud->read();
+        break;
 
-    
-    //case update
-
-    //case delete
-
-    default:
-    $rows = $crud->read();
-    break;
-
+        default:
+            $rows = $crud->read();
+            break;
     }
+}else{
+    $rows = $crud->read();
 }
 
+
 ?>
+
+
+
 
 
 <!DOCTYPE html>
@@ -37,158 +50,176 @@ if(isset($_GET['action'])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>crud</title>
-</head>
-<body>
-  <style>
+    <title>CRUD</title>
+    <style>
         form{
             max-width:500px;
             margin: 0 auto;
         }
-         label{
+        label{
             display: flex;
-            margin-top:10px
-         }
-         input[type=text]{
-            width:100%;
+            margin-top: 10px;
+        }
+        input[type=text]{
+            width: 100%;
             padding: 12px 20px;
             margin: 8px 0;
-            display:inline-block;
+            display: inline-block;
             border: 1px solid #ccc;
-            border-radius:4px;
-            box-sizing:border-box;
-         }
-         input[type=submit]{
-            background-color:#4caf50;
-            color:white;
-            padding:12px 20px;
-            border:none;
-            border-radius:4px;
+            border-radius: 4px;
+            box-sizing: border-box;
+        }
+        input[type=submit]{
+            background-color: #4caf50;
+            color: white;
+            padding: 12px 20px;
+            border: none;
+            border-radius: 4px;
             cursor:pointer;
-            float:right;
-         }
-         input[type=submit]:hover{
-            background-color:#45a049;
-         }
-         table{
-            border-collapse:collapse;
-            width:100%;
-            font-family:Arial, sans-serif;
-            font-size:14px;
-            color:#333;
-         }
-         th, td{
-            text-align:left;
-            padding:8px;
+            float: right;
+        }
+        input [type=submit]:hover{
+            background-color: #45a049;
+        }
+        table{
+            border-collapse: collapse;
+            width: 100%;
+            font-family: Arial, Helvetica, sans-serif;
+            font-size: 14px;
+            color:#333
+        }
+        th,td{
+            text-align: left;
+            padding: 8px;
             border: 1px solid #ddd;
-         }
+        }
         th{
-           background-color:#f2f2f2;
-           font-weight:bold; 
+            background-color: #f2f2f2f2;
+            font-weight: bold;
         }
         a{
-            display:inline-block;
-            padding:4px 8px;
+            display: inline-block;
+            padding: 4px 8px;
             background-color: #007bff;
-            color:#fff;
-            text-decoration:none;
-            border-radius:4px;
+            color: #fff;
+            text-decoration: none;
+            border-radius: 4px;
         }
         a:hover{
-            background-color:#0069d9;
+            background-color: #0069d9;
         }
-
         a.delete{
             background-color: #dc3545;
         }
         a.delete:hover{
-            background-color:#c82333;
+            background-color: #c82333;
         }
     </style>
-</body>
-</html>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>crud</title>
 </head>
 <body>
 
 <?php
-if(isset($_GET['action']) && $_GET['action'] == 'update' && isset($_GET['id'])){
-    $id = $_GET['id'];
-    $result =$crud->readOne($id);
 
-    if ($result){
-        echo "Registro não encontrado.";
-        exit();
-    }
-}
+        if(isset($_GET['action'])&& $_GET['action'] == 'update' && isset ($_GET['id'])){
+            $id = $_GET['id'];
+            $result = $crud->readOne($id);
 
-
-
-
-
-
+            if(!$result){
+                echo "Registro não encontrado.";
+                exit();
+            }
+            $modelo = $result ['modelo'];
+            $marca = $result ['marca'];
+            $placa = $result ['placa'];
+            $cor = $result ['cor'];
+            $ano = $result ['ano'];
+        
+          
 ?>
-    <form action="?action=create" method="POST">
 
+<form action="?action=update" method="POST">
+    <input type="hidden" name="id" value="<?php echo $id ?>">
+    <label for="modelo">Modelo</label>
+    <input type="" name="modelo" value="<?php echo $modelo?>">
+
+    <label for="marca">Marca</label>
+    <input type="text" name="marca" value="<?php echo $marca ?>">
+
+    <label for="placa">Placa</label>
+    <input type="text" name="placa" value="<?php echo $placa ?>">
+
+    <label for="cor">Cor</label>
+    <input type="text" name="cor" value="<?php echo $cor ?>">
+
+    
+    <label for="ano">Ano</label>
+    <input type="text" name="ano" value="<?php echo $ano ?>">
+
+    <input type="submit" value = "Atualizar" name="enviar" onclick= "return confirm('certeza que deseja atualizar?')">
+</form>
+
+<?php
+        }else{
+
+        
+?>
+
+
+    <form action="?action=create" method="POST">
         <label for="">Modelo</label>
-         <input type="text" name="modelo">
+        <input type="text" name="modelo">
 
         <label for="">Marca</label>
         <input type="text" name="marca">
 
         <label for="">Placa</label>
-         <input type="text" name="placa">
+        <input type="text" name="placa">
 
         <label for="">Cor</label>
-         <input type="text" name="cor">
+        <input type="text" name="cor">
 
         <label for="">Ano</label>
-         <input type="text" name="ano">
+        <input type="text" name="ano">
 
-          <input type="submit" value="cadastrar" name="enviar"></input>
-          
+        <input type="submit" value="Cadastrar" name="enviar">
     </form>
-
-   
-          <table>
-     <tr>
-            <td>id</td>
-            <td>modelo</td>
-            <td>marca</td>
-            <td>placa</td>
-            <td>cor</td>
-            <td>ano</td>
-            <td> ações</td>
-    </tr>
     <?php
-        if (isset($rows)){
+        }
+    ?>
+
+    <table>
+        <tr>
+            <td>Id</td>
+            <td>Modelo</td>
+            <td>Marca</td>
+            <td>Placa</td>
+            <td>Cor</td>
+            <td>Ano</td>
+            <td>Ações</td>
+        </tr>
+        <?php
+        if(isset($rows)){
             foreach($rows as $row){
-                echo"<tr>";
-                echo"<td>". $row['id']."</td>";
-                echo"<td>". $row['marca']."</td>";
-                echo"<td>". $row['modelo']."</td>";
-                echo"<td>". $row['placa']."</td>";
-                echo"<td>". $row['cor']."</td>";
-                echo"<td>". $row['ano']."</td>";
-                echo"<td>";
-                echo"<a href='?action=update&id=".$row['id']."'>Editar</a>";
-                echo"<a href='?action=delete&id=".$row['id']."'onclick='return confirm(\"Tem certeza que deseja deletar esse registro?\")'class='delete'>Deletar</a>";
-                echo"</td>";
-                echo"</tr>";
+                echo "<tr>";
+                echo "<td>". $row ['id']. "</td>";
+                echo "<td>". $row ['modelo']. "</td>";
+                echo "<td>". $row ['marca']. "</td>";
+                echo "<td>". $row ['placa']. "</td>";
+                echo "<td>". $row ['cor']. "</td>";
+                echo "<td>". $row ['ano']. "</td>";
+                echo "<td>";
+                echo "<a href ='?action=update&id=" .$row ['id']."'>Editar</a>";
+                echo "<a href ='?action=delete&id=" .$row ['id']."'onclick='return confirm(\"Tem certeza que deseja deletar esse registro?\")' class = 'delete'>Deletar</a>";
+                echo "</td>";
+                echo "</tr>";
 
             }
 
         }else{
-            echo "não há registros a serem exibidos";
-        }
-    ?>
-          </table>
-
+                echo "Não há registros a serem exibidos";
+            }
+        
+        ?>
+    </table>
 </body>
 </html>
